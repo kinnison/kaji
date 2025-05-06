8dd833c823121473925270c945ba085fbf3535f6
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-05-06 20:39:11 +0100

    kaji-rules: Improve Fish technique to be more generic over size
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- kaji-rules/src/techniques/fish.rs
+++ kaji-rules/src/techniques/fish.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+use itertools::Itertools;
 use kaji::{LogicalStep, SolveState, SymbolSetId, Technique};
 
 #[derive(Debug)]
@@ -35,94 +36,96 @@ impl Technique for Fish {
             .filter(|region| state.region(*region).len() == house_size)
             .collect::<Vec<_>>();
         for symbol in state.symbols(self.set) {
-            for (ridx1, region1) in regions.iter().copied().enumerate() {
-                let mut possible_cells1 = HashSet::new();
-                for cell in state.region(region1).to_cells() {
-                    let choice = state.choices(cell, self.set);
-                    if !choice.solved() && choice.can_be(symbol) {
-                        possible_cells1.insert(cell);
+            'region_set: for regions_to_use in regions.iter().copied().combinations(self.size) {
+                let possible_cells = regions_to_use
+                    .iter()
+                    .copied()
+                    .map(|region| {
+                        state
+                            .region(region)
+                            .to_cells()
+                            .into_iter()
+                            .filter(|&cell| {
+                                let choice = state.choices(cell, self.set);
+                                !choice.solved() && choice.can_be(symbol)
+                            })
+                            .collect::<HashSet<_>>()
+                    })
+                    .collect::<Vec<_>>();
+                // Check if all regions have the right possibility count
+                if possible_cells.iter().any(|r| r.len() != self.size) {
+                    continue 'region_set;
+                }
+                for (r1, r2) in possible_cells.iter().tuple_combinations() {
+                    // Check if any two of the regions picked have overlapping cells
+                    if r1.intersection(r2).count() > 0 {
+                        continue 'region_set;
+                    }
+                    // No overlapping cells, do we have unique seeing pairs?
+                    let mut seen = HashSet::new();
+                    for r1cell in r1.iter().copied() {
+                        let mut cell_seen = None;
+                        for r2cell in r2.iter().copied() {
+                            if state.can_see(r1cell, symbol, r2cell, symbol) {
+                                if cell_seen.is_some() {
+                                    // Overlapped
+                                    continue 'region_set;
+                                }
+                                cell_seen = Some(r2cell);
+                            }
+                        }
+                        seen.extend(cell_seen);
+                    }
+                    if &seen != r2 {
+                        // We did not see every cell in r2 from the cells in r1
+                        continue 'region_set;
                     }
                 }
-                if possible_cells1.len() != self.size {
-                    // This region is not suitable
-                    continue;
-                }
-                'region2: for region2 in regions[ridx1 + 1..].iter().copied() {
-                    let mut possible_cells2 = HashSet::new();
-                    for cell in state.region(region2).to_cells() {
-                        let choice = state.choices(cell, self.set);
-                        if !choice.solved() && choice.can_be(symbol) {
-                            possible_cells2.insert(cell);
-                        }
-                    }
-                    if possible_cells2.len() != self.size {
-                        // This region is not suitable
-                        continue;
-                    }
-                    if possible_cells1.intersection(&possible_cells2).count() > 0 {
-                        // The regions managed to overlap somehow
-                        continue;
-                    }
-                    // Here we know:
-                    // (a) region1 and region2 are of the correct size
-                    // (b) they both contain self.size cells which could be symbol
-                    // Next determine if we have the fish, ie for each cell in region1
-                    // is there a single unique cell in region2 which it can see?
-                    let mut saw2 = HashSet::new();
-                    let mut region1_sees = HashSet::new();
-                    for cell in possible_cells1.iter().copied() {
-                        let sees = state.sees(cell, symbol).collect::<HashSet<_>>();
-                        let saw = possible_cells2
-                            .intersection(&sees)
-                            .copied()
-                            .collect::<Vec<_>>();
-                        if saw.len() != 1 {
-                            // We saw either too much, or too little, let's carry on
-                            continue 'region2;
-                        }
-                        if saw2.contains(&saw[0]) {
-                            // We already saw this
-                            continue 'region2;
-                        }
-                        saw2.insert(saw[0]);
-                        region1_sees.extend(sees);
-                    }
-                    // Now we have region1_sees which is all the cells region1's cells can see
-                    for cell in possible_cells1.iter() {
-                        region1_sees.remove(cell);
-                    }
-                    let mut region2_sees =
-                        possible_cells2
-                            .iter()
-                            .copied()
-                            .fold(HashSet::new(), |mut acc, cell| {
-                                acc.extend(state.sees(cell, symbol));
-                                acc
-                            });
-                    for cell in possible_cells2.iter() {
-                        region2_sees.remove(cell);
-                    }
-                    // Now we have region1_sees and region2_sees
-                    let possible_elim_cells = region1_sees.intersection(&region2_sees);
-                    let mut action = LogicalStep::action(format!("{} on ", fish_name(self.size)));
-                    action.push_symbols(Some(state.symbol(symbol)));
-                    action.push_str(" at ");
-                    action.push_cells(possible_cells1.iter().map(|c| state.cell_info(*c)));
+                // Now we know:
+                // We have the right number of regions
+                // Each region has the right number of possibilities
+                // Each cell in a region sees a unique set of cells in each other region
+                // Which means we can perform the union/intersection work
+                let sees = possible_cells
+                    .iter()
+                    .map(|rcells| {
+                        let mut acc =
+                            rcells
+                                .iter()
+                                .copied()
+                                .fold(HashSet::new(), |mut acc, cell| {
+                                    acc.extend(state.sees(cell, symbol));
+                                    acc
+                                });
+                        rcells.iter().for_each(|cell| {
+                            acc.remove(cell);
+                        });
+                        acc
+                    })
+                    .reduce(|acc, elem| acc.intersection(&elem).copied().collect())
+                    .expect("No region cells?");
+                // sees is now the set of cells seen by all the unions of the input region cells
+
+                let mut action = LogicalStep::action(format!("{} on ", fish_name(self.size)));
+                action.push_symbols(Some(state.symbol(symbol)));
+                action.push_str(" at ");
+                action.push_cells(possible_cells[0].iter().map(|c| state.cell_info(*c)));
+                for possible_cells2 in possible_cells[1..].iter() {
                     action.push_str(" and ");
                     action.push_cells(possible_cells2.iter().map(|c| state.cell_info(*c)));
-                    action.push_str(" eliminates ");
-                    action.push_symbols(Some(state.symbol(symbol)));
-                    action.push_str(" from ");
-                    let mut cleared = Vec::new();
-                    for cell in possible_elim_cells.copied() {
-                        if state.eliminate(cell, symbol).changed() {
-                            cleared.push(cell);
-                        }
+                }
+                action.push_str(" eliminates ");
+                action.push_symbols(Some(state.symbol(symbol)));
+                action.push_str(" from ");
+                let mut cleared = Vec::new();
+                for cell in sees.into_iter() {
+                    if state.eliminate(cell, symbol).changed() {
+                        cleared.push(cell);
                     }
-                    if !cleared.is_empty() {
-                        action.push_cells(cleared.into_iter().map(|cell| state.cell_info(cell)));
-                        return action;
-                    }
+                }
+                if !cleared.is_empty() {
+                    action.push_cells(cleared.into_iter().map(|cell| state.cell_info(cell)));
+                    return action;
                 }
             }
         }
